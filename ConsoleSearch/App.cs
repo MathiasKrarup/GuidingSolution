4c1a4e63663d14b3cad2de64ca8db4dddc8f1d89
Matty <Mathiaskrarupmadsen@gmail.com>
2024-02-20 02:00:00 +0100

    Initial commit

---
+++ ConsoleSearch/App.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleSearch
+{
+    public class App
+    {
+        public async void Run()
+        {
+            SearchLogic mSearchLogic = new SearchLogic();
+            Console.WriteLine("Console Search");
+            
+            while (true)
+            {
+                Console.WriteLine("enter search terms - q for quit [default: hello]");
+                string input = Console.ReadLine() ?? "hello"; // Search for hello by default
+                if (input.Equals("q")) break;
+
+                var wordIds = new List<int>();
+                var searchTerms = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (var t in searchTerms)
+                {
+                    int id = mSearchLogic.GetIdOf(t);
+                    if (id != -1)
+                    {
+                        wordIds.Add(id);
+                    }
+                    else
+                    {
+                        Console.WriteLine(t + " will be ignored");
+                    }
+                }
+
+                DateTime start = DateTime.Now;
+
+                var docIds = await mSearchLogic.GetDocuments(wordIds);
+                
+                var top10 = new List<int>();
+                foreach (var p in docIds.Take(10))
+                {
+                    top10.Add(p.Key);
+                }
+
+                TimeSpan used = DateTime.Now - start;
+
+                int idx = 0;
+                foreach (var doc in await mSearchLogic.GetDocumentDetails(top10))
+                {
+                    Console.WriteLine("" + (idx+1) + ": " + doc + " -- contains " + docIds[docIds.Keys.ToArray()[idx]] + " search terms");
+                    idx++;
+                }
+                Console.WriteLine("Documents: " + docIds.Count + ". Time: " + used.TotalMilliseconds);
+                
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ ConsoleSearch/SearchLogic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleSearch
+{
+    public class SearchLogic
+    {
+        private HttpClient api = new() { BaseAddress = new Uri("http://word-service") };
+
+        Dictionary<string, int> mWords;
+
+        public SearchLogic()
+        {
+            var response = api.Send(new HttpRequestMessage(HttpMethod.Get, "Word"));
+            var content = response.Content.ReadAsStringAsync().Result;
+            mWords = JsonSerializer.Deserialize<Dictionary<string, int>>(content);
+        }
+
+        public int GetIdOf(string word)
+        {
+            if (mWords.ContainsKey(word))
+                return mWords[word];
+            return -1;
+        }
+
+        public async Task<Dictionary<int, int>> GetDocuments(List<int> wordIds)
+        {
+            var url = $"Document/GetByWordIds?wordIds={string.Join("&wordIds=", wordIds)}";
+            var response = await api.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+            return JsonSerializer.Deserialize<Dictionary<int, int>>(content);
+        }
+
+        public async Task<List<string>> GetDocumentDetails(List<int> docIds)
+        {
+            var url = $"Document/GetByDocIds?docIds={string.Join("&docIds=", docIds)}";
+            var response = await api.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<string>>(content);
+        }
+    }
+}
